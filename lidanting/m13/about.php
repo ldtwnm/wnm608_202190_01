1174b85670a812d2c4aa1067ca11b5e3ad0ebf7f
Danting Li <liz.danting.li@gmail.com>
2021-12-07 17:25:17 -0800

    footer back to former

--- lidanting/m13/about.php
+++ lidanting/m13/about.php
@@ -28,7 +28,7 @@ include_once "lib/php/functions.php";
 	<div class="container">
 		<div class="card soft">
 			<h2>About Us</h2>
-			<p>YOSN means young mountain in Chinses, which stands for the power of new generation.</p>
+			<p>YOSN means young mountain in Chinses, which stands for the power of new generation. We offer different design services including UX, UI, graphic, branding, video, typography, coding, etc.</p>
 		</div>
 	</div>
 	
